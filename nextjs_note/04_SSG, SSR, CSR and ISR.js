cc58843a0be2c954ade4bfb9e73ec55e5feb8078
Nghiem Pham <nghiemph95@gmail.com>
2023-03-02 16:04:12 +0700

    Updated: 02-03-2023 16:04:12

--- nextjs_note/04_SSG, SSR, CSR and ISR.js
+++ nextjs_note/04_SSG, SSR, CSR and ISR.js
@@ -5,3 +5,15 @@
     . CSR - Client Side Rendering
     . ISR - Incremental Static Regeneration
  */
+
+/** Pre-rendering (SSR - SSG): Render sẵn file HTML ở fía server lên cho user
+ * - Initial Load: Pre-render HTML để hiện thị cho user
+ * - JS Loads: Browser sẽ load file JS lên
+ * - Hydrate: Giống như hàm render(). Nhưng sử dụng để hydrate HTML container reactDOM đã được render
+ * từ server, sau đó client lấy file html đó về và nhiệm vụ của hydrate là thêm event listener vào markup.
+ *
+ */
+
+/** Lưu ý:
+ * Đối với ReactApp: Ban đầu nó chưa render, nó chỉ tải file html có div root là rỗng, sau đó mới load JS và render APP
+ */
