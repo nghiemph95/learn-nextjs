22d7a72f6d0a6f54b3602120b1e487ed2030a16f
Nghiem Pham <nghiemph95@gmail.com>
2023-03-02 15:37:50 +0700

    Updated: 02-03-2023 15:37:50

---
+++ nextjs_note/01_nextJSIntro.js
@@ -0,0 +1,16 @@
+/** NextJS là React Framework
+ *
+ * Những điểm nổi bật:
+ * - Image Optimization: Automatic Image Optimization with instant build
+ * - Internationalization: Built-in Domain & Subdomain Routing and Automatic Language detection
+ * - Next.js Analytics: A true lighthouse score base on real visitor data & page-by-page insight
+ * - Zero Config: Automatic compilation and bundling.
+ * - Hybrid: SSG and SSR (Pre-render pages at build time (SSG) or request time (SSR) in a single project)
+ * - Incremental Static Regeneration: Add and update statically pre-rendered pages incrementally after build time.
+ * - TypeScrip Support
+ * - Fast Refresh: Fast, reliable live-editing experience
+ * - File-system Routing: Every component in the 'page' directory becomes a route.
+ * - API Routes: Optionally create API endpoints to provide backend fucntionally.
+ * - Build-in CSS Support
+ * - Code-spliting and Bundling
+ */
