d6facaca7274c4bea641c1e4a60fb90ea4a94003
Nghiem Pham <nghiemph95@gmail.com>
2023-03-02 14:16:07 +0700

    Updated: 02-03-2023 14:16:07

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,7 +1,12 @@
+import { useRouter } from 'next/router';
 import React from 'react';
 
 export interface AboutPageProps {}
 
 export default function AboutPage(props: AboutPageProps) {
+  const router = useRouter();
+
+  console.log('About query: ', router.query);
+
   return <div>About Page</div>;
 }
