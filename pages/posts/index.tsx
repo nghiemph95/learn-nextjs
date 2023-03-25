5e6fb3bb6c781d35cbabcf1e28584258313ff762
Nghiem Pham <nghiemph95@gmail.com>
2023-03-25 22:51:05 +0700

    Updated: 25-03-2023 22:51:05

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,7 +1,18 @@
+import { GetStaticProps, GetStaticPropsContext } from 'next';
 import React from 'react';
 
-export interface PostListPageProps {}
+export interface PostListPageProps {
+  posts: any[];
+}
 
 export default function PostListPage(props: PostListPageProps) {
   return <div>Post List Page</div>;
 }
+
+export const getStaticProps: GetStaticProps<PostListPageProps> = async (context: GetStaticPropsContext) => {
+  return {
+    props: {
+      posts: [],
+    },
+  };
+};
